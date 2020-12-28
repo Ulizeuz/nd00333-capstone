49b224e46aca2197f9cf58c80c463ebee6a2db3f
Ulises Gomez Olguin <59584816+Ulizeuz@users.noreply.github.com>
2020-12-28 05:43:18 +0000

    clean train.py

--- train.py
+++ train.py
@@ -1,42 +1,21 @@
-from sklearn.linear_model import LogisticRegression #1
-import argparse #1
+from sklearn.linear_model import LogisticRegression
+import argparse
 import os
-import numpy as np #1
+import numpy as np
 from sklearn.metrics import mean_squared_error
-import joblib #1
+import joblib
 from sklearn.model_selection import train_test_split
-# from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run#1
 from azureml.data.dataset_factory import TabularDatasetFactory
 from azureml.core import Dataset
 
 # TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-run = Run.get_context() #1
 
-def clean_data(data):#1
+run = Run.get_context()
 
-    # Clean and one hot encode data
-    # x_df = data.to_pandas_dataframe().dropna()
-    # jobs = pd.get_dummies(x_df.job, prefix="job")
-    # x_df.drop("job", inplace=True, axis=1)
-    # x_df = x_df.join(jobs)
-    # x_df["marital"] = x_df.marital.apply(lambda s: 1 if s == "married" else 0)
-    # x_df["default"] = x_df.default.apply(lambda s: 1 if s == "yes" else 0)
-    # x_df["housing"] = x_df.housing.apply(lambda s: 1 if s == "yes" else 0)
-    # x_df["loan"] = x_df.loan.apply(lambda s: 1 if s == "yes" else 0)
-    # contact = pd.get_dummies(x_df.contact, prefix="contact")
-    # x_df.drop("contact", inplace=True, axis=1)
-    # x_df = x_df.join(contact)
-    # education = pd.get_dummies(x_df.education, prefix="education")
-    # x_df.drop("education", inplace=True, axis=1)
-    # x_df = x_df.join(education)
-    # x_df["month"] = x_df.month.map(months)
-    # x_df["day_of_week"] = x_df.day_of_week.map(weekdays)
-    # x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
-    
+def clean_data(data):
+   
     x_df = data.to_pandas_dataframe()
     
     y_df = x_df.pop("DEATH_EVENT")
@@ -47,32 +26,32 @@ data = pd.read_csv("heart_failure_clinical_records_dataset.csv")
 
 ds = Dataset.Tabular.from_delimited_files(data)
 
-x, y = clean_data(ds) #1
+x, y = clean_data(ds)
 
 # TODO: Split data into train and test sets.
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=403,shuffle=True) # 1 
 
-run = Run.get_context() #1
+run = Run.get_context()
 
 def main():
     # Add arguments to script
-    parser = argparse.ArgumentParser() #1
+    parser = argparse.ArgumentParser()
 
-    parser.add_argument('-f') #1
+    parser.add_argument('-f')
     
-    parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization") #1
-    parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge") #1
+    parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
+    parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
 
-    primary_metric_name="Accuracy" #1
-    args = parser.parse_args() #1
+    primary_metric_name="Accuracy"
+    args = parser.parse_args()
 
-    run.log("Regularization Strength:", np.float(args.C)) #1
-    run.log("Max iterations:", np.int(args.max_iter)) #1 
+    run.log("Regularization Strength:", np.float(args.C))
+    run.log("Max iterations:", np.int(args.max_iter))
 
-    model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train) #1
-    joblib.dump(model,'outputs/model.joblib') #1
-    accuracy = model.score(x_test, y_test) #1
+    model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
+    joblib.dump(model,'outputs/model.joblib')
+    accuracy = model.score(x_test, y_test)
     
-    run.log("Accuracy", np.float(accuracy)) #1
-if __name__ == '__main__': #1
-    main() #1
+    run.log("Accuracy", np.float(accuracy))
+if __name__ == '__main__':
+    main()
