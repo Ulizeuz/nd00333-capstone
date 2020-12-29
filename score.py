7836e925bca1337175f1395d1dac0bfa5be39449
Ulises Gomez Olguin <59584816+Ulizeuz@users.noreply.github.com>
2020-12-28 18:10:34 -0600

    Update score.py

--- score.py
+++ score.py
@@ -1,27 +1,17 @@
-import joblib
 import json
 import numpy as np
 import os
-
-from inference_schema.schema_decorators import input_schema, output_schema
-from inference_schema.parameter_types.numpy_parameter_type import NumpyParameterType
-
+from sklearn.externals import joblib
 def init():
     global model
     model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'automl_hearth.pkl')
+    print("Found model:", os.path.isfile(model_path)) #To check whether the model is actually present on the location we are looking at
     model = joblib.load(model_path)
-
-input_sample = np.array([[75, 0, 582, 0, 20, 1, 265000, 1.9, 130, 1, 0, 4, 1]])
-output_sample = np.array([0])
-
-@input_schema('data', NumpyParameterType(input_sample))
-@output_schema(NumpyParameterType(output_sample))
-
 def run(data):
     try:
+        data = np.array(json.loads(data))
         result = model.predict(data)
-
-        return "Here is your result = " + str(result)
+        return result.tolist()
     except Exception as e:
         error = str(e)
         return error
