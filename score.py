b827ea4e50d5e4abdcd41cb163b482d4f316dc5a
Ulises Gomez Olguin <59584816+Ulizeuz@users.noreply.github.com>
2021-01-04 21:11:28 +0000

    fix must pass 2-d input

--- score.py
+++ score.py
@@ -9,8 +9,11 @@ def init():
     model = joblib.load(model_path)
 def run(data):
     try:
-        data = np.array(json.loads(data))
+        #data = np.array(json.loads(data))
+        data = json.loads(data)['data']
+        data = pd.DataFrame.from_dict(data)
         result = model.predict(data)
+        # You can return any data type, as long as it is JSON serializable.
         return result.tolist()
     except Exception as e:
         error = str(e)
