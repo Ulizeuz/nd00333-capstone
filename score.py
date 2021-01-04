ca7c244f16425453305356406db96fbaa252af8c
Ulises Gomez Olguin <59584816+Ulizeuz@users.noreply.github.com>
2021-01-04 21:29:53 +0000

    Update score.py

--- score.py
+++ score.py
@@ -2,6 +2,8 @@ import json
 import numpy as np
 import os
 import joblib
+import pandas as pd
+
 def init():
     global model
     model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'automl_hearth.pkl')
